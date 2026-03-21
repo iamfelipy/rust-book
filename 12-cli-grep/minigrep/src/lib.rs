0c59e4c4731bbc1cd7d37b3739f09c67430c4744
felipy <felipy81br@gmail.com>
2026-03-21 08:25:53 +0000

    feat: initialize minigrep project with basic functionality
    - Added Cargo.toml and Cargo.lock for project metadata and dependency management.
    - Implemented main.rs for command-line argument parsing and file reading.
    - Created lib.rs for shared functionality and configuration handling.
    - Included a sample poem in poem.txt for testing purposes.

---
+++ 12-cli-grep/minigrep/src/lib.rs
@@ -0,0 +1,32 @@
+// lib.rs
+// Isso permite ter funções compartilhadas entre binário e biblioteca no mesmo projeto.
+use std::error::Error;
+use std::fs;
+
+pub struct Config {
+  pub query: String,
+  pub file_path: String,
+}
+
+impl Config {                                  // string literal
+  pub fn build(args: &[String]) -> Result<Config, &'static str> {
+      
+      if args.len() < 3 {
+          return Err("not enough arguments");
+      }
+
+      let query = args[1].clone();
+      let file_path = args[2].clone();
+
+      Ok(Config { query, file_path })
+  }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+  // O operador ? retorna imediatamente do bloco atual caso o valor seja Err (ou None), propagando o erro para o chamador; caso contrário, desempacota o valor de sucesso.
+  let contents = fs::read_to_string(config.file_path)?;
+
+  println!("With text:\n{contents}");
+
+  Ok(())
+}
---
+++ 12-cli-grep/minigrep/src/main.rs
@@ -0,0 +1,28 @@
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+fn main() {
+    //collect:  permitindo acesso indexado aos argumentos da linha de comando.
+    let args: Vec<String> = env::args().collect();
+    // dbg!(args);
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    
+    // Você consegue importar minigrep porque em projetos Rust, o próprio nome do pacote é usado como crate interno (a partir do nome no Cargo.toml). Assim, o código em src/lib.rs fica disponível via minigrep para o binário em src/main.rs
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
+
+
+
